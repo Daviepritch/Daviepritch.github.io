721198609155b90e6c03236f86b890fd005718e3
Daviepritch <100312101+Daviepritch@users.noreply.github.com>
2023-10-06 14:35:58 +0100

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,17 +1,12 @@
-<header class="navbar navbar-default navbar-static-top">
-    <div class="container-fluid">
-        <div class="navbar-header">
-            <a href="#" class="navbar-brand"></a>
-        </div>
-        <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>><a href="index">Home</a></li>                    
+<nav>
+  <ul class="navigation">
+
     <li style="float:left"><a href="daviepritch.github.io/index.html" style="text-decoration:none;">Main Page</a></li>
     <li style="float:left"><a href="daviepritch.github.io/main/form.html" style="text-decoration:none;">Form</a></li>
     <li style="float:left"><a href="daviepritch.github.io/main/table.html" style="text-decoration:none;">Pricing</a></li>
 	<li style="float:right"><a href="signup.html" style="text-decoration:none;">Sign Up</a></li>
-	<li style="float:right"><a href="login.html" style="text-decoration:none;">Sign In</a></li>
-            </ul>
-        </div>
-    </div>
-</header>
+	<li style="float:right"><a href="main/login.html" style="text-decoration:none;">Sign In</a></li>
+
+
+  </ul>
+  </nav>
